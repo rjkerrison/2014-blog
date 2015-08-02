98387ec60df5147d64000e0ed961e5f4f64f9faf
rjkerrison <rjk1994@gmail.com>
2015-08-02 22:32:44 +0100

    element-specific doubletapping

--- _script/doubletap.js
+++ _script/doubletap.js
@@ -6,26 +6,28 @@ var $document = $(document);
 var distanceThreshold = 5;
 var timeThreshold = 1000;
 
-$document.mousedown(function ( firstDown ) {
-	$document.mouseup(function ( firstUp ) {
-		$document.off('mouseup');
+$('*').mousedown(function ( firstDown ) {
+	$(this).mouseup(function ( firstUp ) {
+		firstUp.stopPropagation();
+		$(this).off('mouseup');
 
 		if (Math.abs(firstUp.pageX - firstDown.pageX) < distanceThreshold
 			&& Math.abs(firstUp.pageX - firstDown.pageX) < distanceThreshold) {
 
-			var trigger = $document.mousedown(function ( secondDown ) {
-				$document.off('mousedown', trigger);
+			var trigger = $(this).mousedown(function ( secondDown ) {
+				$(this).off('mousedown', trigger);
 
 				if (Math.abs(firstUp.pageX - secondDown.pageX) < distanceThreshold
 					&& Math.abs(firstUp.pageX - secondDown.pageX) < distanceThreshold) {
 
-					$document.mouseup(function ( secondUp ) {
-						$document.off('mouseup');
+					$(this).mouseup(function ( secondUp ) {
+						secondUp.stopPropagation();
+						$(this).off('mouseup');
 
 						if (secondUp.timeStamp - firstDown.timeStamp < timeThreshold
 							&& Math.abs(secondUp.pageX - secondDown.pageX) < distanceThreshold
 							&& Math.abs(secondUp.pageX - secondDown.pageX) < distanceThreshold) {
-							$document.trigger('doubletap');
+							$(this).trigger('doubletap');
 						};
 					});
 				};
