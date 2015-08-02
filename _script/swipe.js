4c5f4be3b816284c184de52686815fa6ac11e636
rjkerrison <rjk1994@gmail.com>
2015-08-02 21:31:43 +0100

    swipe is triggered on least containing element

--- _script/swipe.js
+++ _script/swipe.js
@@ -6,13 +6,14 @@ var $document = $(document);
 var horizontalThreshold = 30;
 var timeThreshold = 1000;
 
-$document.mousedown(function (e) {
-	$document.mouseup(function ( mouseup ) {
-		$document.off('mouseup');
-		if (mouseup.timeStamp - e.timeStamp < timeThreshold
-			&& Math.abs(mouseup.pageX - e.pageX) > horizontalThreshold
-			&& Math.abs(mouseup.pageX - e.pageX) > Math.abs(mouseup.pageY - e.pageY)) {
-			$document.trigger('swipe');
+$('*').mousedown(function ( mousedown ) {
+	$(this).mouseup(function ( mouseup ) {
+		mouseup.stopPropagation();
+		$(this).off('mouseup');
+		if (mouseup.timeStamp - mousedown.timeStamp < timeThreshold
+			&& Math.abs(mouseup.pageX - mousedown.pageX) > horizontalThreshold
+			&& Math.abs(mouseup.pageX - mousedown.pageX) > Math.abs(mouseup.pageY - mousedown.pageY)) {
+			$(this).trigger('swipe');
 		}
-	})
+	});
 });
